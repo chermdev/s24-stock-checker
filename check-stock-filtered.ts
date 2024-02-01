df811461beab875725661e940d2bd826e093a8eb
CharlyyShell <chermdev@gmail.com>
2024-02-01 12:56:30 -0600

    refactor: import pdp urls from json file

--- check-stock-filtered.ts
+++ check-stock-filtered.ts
@@ -1,14 +1,8 @@
 import { fetchStock } from './fetch-stock/fetch-stock.ts'
-import { SamsungSKU } from './fetch-stock/skus.ts'
-
-const pdpUrls = [
-  "https://www.samsung.com/mx/mobile-accessories/galaxy-s24-ultra-anti-reflecting-screen-protector-transparent-ef-us928ctegmx",
-  "https://www.samsung.com/mx/mobile-accessories/galaxy-s24-ultra-clear-case-transparent-gp-fps928saatx/",
-  SamsungSKU.ultraNegro512GB,
-  SamsungSKU.ultraGris512GB]
+import products from './products.json'
 
 async function checkStockFromUrls() {
-  const stock = await fetchStock(pdpUrls)
+  const stock = await fetchStock(products)
 
   console.table(stock.filter((item) => (
     item.stockStatus !== 'outOfStock')
